6be62066ed0165518cd7fe17497dbdf66229c09a
m182s <johnmier182@gmail.com>
2024-04-24 15:45:20 +0800

    add delete icon

--- app/src/main/java/com/example/homepage/MyViewHolder.java
+++ app/src/main/java/com/example/homepage/MyViewHolder.java
@@ -1,6 +1,7 @@
 package com.example.homepage;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,9 +10,12 @@ public class MyViewHolder extends RecyclerView.ViewHolder
 {
     public TextView itemNameTextView;
     public TextView priceTextView;
+
+    public ImageView iconView;
     public MyViewHolder(View itemView) {
         super(itemView);
         itemNameTextView = itemView.findViewById(R.id.itemName);
         priceTextView = itemView.findViewById(R.id.price);
+        iconView = itemView.findViewById(R.id.del_icon);
     }
 }
